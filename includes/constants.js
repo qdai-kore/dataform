8bf001981caaac3502dd75a5acf563c2eb0f1f64
Quan Dai <qdai@korewireless.com>
2023-11-03 14:33:57 +0000

    make last n min as variable and created new method to handle last n min and use it in twilio eom

--- includes/constants.js
+++ includes/constants.js
@@ -1,10 +1,8 @@
 // myDateUtils.js
 
-// Function to get the start of the previous day
-// if today is 2023-07-14 08:00:01 UTC time, then returns '2023-07-13 00:00:00.000 UTC'
-function getStartOfPreviousMin(min) {
+// current time - x min
+function getPreviousMin(min) {
   const currentDate = new Date();
-  currentDate.setHours(0, 0, 0, 0);
   const previousDate = new Date(currentDate.getTime() - min * 60 * 1000);
   // Return the timestamp
   const formattedDate = previousDate.toISOString().replace('T', ' ').replace('Z', ' UTC');
@@ -14,12 +12,17 @@ function getStartOfPreviousMin(min) {
 // Function to get the start of the previous day
 // if today is 2023-07-14 08:00:01 UTC time, then returns '2023-07-13 00:00:00.000 UTC'
 function getStartOfPreviousDay(numDays) {
-  return getStartOfPreviousMin(numDays * 24 * 60);
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+  const previousDate = new Date(currentDate.getTime() - numDays * 24 * 60 * 60 * 1000);
+  // Return the timestamp
+  const formattedDate = previousDate.toISOString().replace('T', ' ').replace('Z', ' UTC');
+  return formattedDate;
 };
 
 
 
 module.exports = {
-  getStartOfPreviousDay,getStartOfPreviousMin
+  getStartOfPreviousDay,getPreviousMin
 };
 
